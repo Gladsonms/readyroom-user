98843ce8f79d7f87f5f7bc05afcc958e03dbf318
unknown <gladsonsaji4@gmail.com>
2022-01-07 11:43:55 +0530

    added naviagtion bar added ant desigen icon

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login/Login";
 import Signup from "./Pages/Signup/Signup";
 import TopNav from "./Components/TopNav/TopNav";
+import "./App.css";
 
 function App() {
   return (
--- src/Components/TopNav/TopNav.js
+++ src/Components/TopNav/TopNav.js
@@ -1,17 +1,34 @@
-import { Link } from "react-router-dom";
-
-const TopNav = () => (
-  <div className="nav bg-dark d-flex justify-content-between">
-    <Link className="nav-link" to="/">
-      Home
-    </Link>
-    <Link className="nav-link" to="/login">
-      Login
-    </Link>
-    <Link className="nav-link" to="/booking">
-      Booking
-    </Link>
-  </div>
-);
+import React, { useState } from "react";
+import "./TopNav.css";
+import { SearchOutlined, MenuFoldOutlined } from "@ant-design/icons";
+function TopNav() {
+  const [showLinks, setshowLinks] = useState(true);
+  return (
+    <div className="navbar">
+      <div className="leftside">
+        <div className="links" id={showLinks ? "hidden" : ""}>
+          <a to="/">Home</a>
+          <a to="/">Hotels</a>
+          <a to="/booking">Booking</a>
+          <a to="/login">Login</a>
+        </div>
+        <button
+          className="navButton"
+          onClick={() => {
+            setshowLinks(!showLinks);
+          }}
+        >
+          <MenuFoldOutlined />
+        </button>
+      </div>
+      <div className="rightSide">
+        <input type="text" placeholder="Serach...." />
+        <button>
+          <SearchOutlined />
+        </button>
+      </div>
+    </div>
+  );
+}
 
 export default TopNav;
