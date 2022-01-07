91fd8dc523291f1980699d1784783034cad7aa12
unknown <gladsonsaji4@gmail.com>
2022-01-07 19:29:01 +0530

    added login page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,70 @@
 import React from "react";
 import TopNav from "../../Components/TopNav/TopNav";
+import { Link } from "react-router-dom";
 
 function Login() {
   return (
     <>
       {/* <TopNav iconShow={false} /> */}
-      <div className="container-fluid h1 p-5 text-center">Login</div>
+      <div className="outer-div">
+        <div className="global-container">
+          <div className="card login-form">
+            <div className="card-body">
+              <h1 className="card-title text-center">Login</h1>
+              <div className="card-text">
+                <form action="">
+                  {/* Email */}
+                  <div className="form-group">
+                    <label for="exampleInputEmail" className="form-label">
+                      email
+                    </label>
+                    <input
+                      type="text"
+                      id="email"
+                      className="form-control form-control-sm"
+                      name="email"
+                      placeholder="Enter Your email.."
+                    />
+                  </div>
+                  {/* Phone */}
+
+                  {/* password */}
+                  <div className="form-group">
+                    <label for="exampleInputpassword" className="form-label">
+                      Password
+                    </label>
+                    <input
+                      type="text"
+                      id="password"
+                      className="form-control form-control-sm"
+                      name="password"
+                      placeholder="Enter Your password.."
+                    />
+                  </div>
+                  {/*confirm  password */}
+                  <div className="form-group">
+                    <button type="submit" className="btn btn-primary btn-block">
+                      Login
+                    </button>
+                    <div className="signin">
+                      Dont have an account
+                      <Link className="login-user" to="/Signup">
+                        Signup
+                      </Link>
+                    </div>
+                    <div className="signin">
+                      Login With Otp
+                      <Link className="login-user" to="/Signup">
+                        Login
+                      </Link>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -1,84 +1,8 @@
 import React from "react";
 import "./Signup.css";
+import { Link } from "react-router-dom";
 function Signup() {
   return (
-    // <div className="form-content-right">
-    //   <div className="form">
-    //     <h1>Create Your account filling out the information below</h1>
-    //     {/* Username */}
-    //     <div className="form-inputs">
-    //       <label htmlFor="username" className="form-label">
-    //         Useraname
-    //       </label>
-    //       <input
-    //         type="text"
-    //         id="username"
-    //         className="form-input"
-    //         name="username"
-    //         placeholder="Enter Your username.."
-    //       />
-    //     </div>
-    //     {/* Email address */}
-    //     <div className="form-inputs">
-    //       <label htmlFor="email" className="form-label">
-    //         Email
-    //       </label>
-    //       <input
-    //         type="email"
-    //         id="email"
-    //         className="form-input"
-    //         name="email"
-    //         placeholder="Enter Your  email.."
-    //       />
-    //     </div>
-    //     {/* Phone  number */}
-    //     <div className="form-inputs">
-    //       <label htmlFor="email" className="form-label">
-    //         Phone Number
-    //       </label>
-    //       <input
-    //         type="number"
-    //         id="phonenumber"
-    //         className="form-input"
-    //         name="phonenumber"
-    //         placeholder="Enter Your  phone number.."
-    //       />
-    //     </div>
-    //     {/* Password */}
-    //     <div className="form-inputs">
-    //       <label htmlFor="password" className="form-label">
-    //         password
-    //       </label>
-    //       <input
-    //         type="password"
-    //         id="password"
-    //         className="form-input"
-    //         name="password"
-    //         placeholder="Enter Your  password"
-    //       />
-    //     </div>
-    //     {/*Confirm Password */}
-    //     <div className="form-inputs">
-    //       <label htmlFor="confirmpassword" className="form-label">
-    //         password
-    //       </label>
-    //       <input
-    //         type="password"
-    //         id="confirmpassword"
-    //         className="form-input"
-    //         name="confirmpassword"
-    //         placeholder="Enter Your  password again"
-    //       />
-    //     </div>
-    //     {/* submit button */}
-    //     <button className="form-input-btn" type="submit">
-    //       Signup
-    //     </button>
-    //     <span className="input-form-login">
-    //       Already have an account Login <a href="">Here</a>
-    //     </span>
-    //   </div>
-    // </div>
     <div className="outer-div">
       <div className="global-container">
         <div className="card login-form">
@@ -152,7 +76,12 @@ function Signup() {
                   <button type="submit" className="btn btn-primary btn-block">
                     Signup
                   </button>
-                  <div className="signin">Alreday have an account.Login?</div>
+                  <div className="signin">
+                    Alreday have an account
+                    <Link className="login-user" to="/login">
+                      Login
+                    </Link>
+                  </div>
                 </div>
               </form>
             </div>
